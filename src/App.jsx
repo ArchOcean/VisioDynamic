f3b43310caadc8016390ff06fb72508b6cb859ba
ArchOcean <86221964+ArchOcean@users.noreply.github.com>
2023-06-30 21:22:03 +0700

    30/6

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import React from "react";
+import { BiHomeAlt, BiBook, BiCog } from "react-icons/bi";
 import { Canvas } from "./components/canvas";
 import { Form } from "./components/Form";
+import "./index.css";
 
 function App() {
   return (
-    <div className="container">
-      <Canvas className="canvas" />
+    <div>
+      <nav className="fixed top-0 left-0 z-40 w-[6rem] h-screen transition-transform -translate-x-full sm:translate-x-0">
+        <div className="h-full px-3 py-8 overflow-y-auto bg-black ">
+          <ul className="h-full text-white text-4xl flex flex-col items-center place-content-between">
+            <li>
+              <BiHomeAlt className="" />
+            </li>
+            <li>
+              <BiBook className="" />
+            </li>
+            <li>
+              <BiCog className="" />
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <main className="flex-auto h-full bg-white"></main>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: "#1f1f1f",
+      white: "#ffffff",
+    },
+  },
+  plugins: [],
+};
